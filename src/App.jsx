6b735c29b3eb4f89dd1ac9fae84af58cf6591b7f
Anisah <ibrahimanisah45@gmail.com>
2025-07-23 15:40:52 +0300

    Navbar, Landing page updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar/Navbar';
-import Landing from './Components/Landing/Landing'; 
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Landing from "./Components/Landing/Landing";
 
 function App() {
   return (
-    <> <Navbar />
-    <Landing />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<div>Home Page Coming Soon</div>} />
+        <Route path="/genres" element={<div>Genres Page Coming Soon</div>} />
+        <Route path="/favorites" element={<div>Favorites Page Coming Soon</div>} />
+      </Routes>
     </>
-    
-     
   );
 }
 
